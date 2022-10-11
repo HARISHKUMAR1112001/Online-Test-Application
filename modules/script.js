ca4d01c17d047aebe1968dad15ee210aceaaaa53
S1acharya <acharyasaket.08@gmail.com>
2022-10-11 12:55:48 +0530

    Add signup feature

---
+++ modules/script.js
@@ -0,0 +1,184 @@
+let quizData;
+
+if(document.getElementById('html'))
+{
+    quizData =  [
+        {
+            question: "Which language runs in a web browser abhinav?",
+            a: "Java",
+            b: "C",
+            c: "Python",
+            d: "JavaScript",
+            correct: "d",
+        },
+        {
+            question: "What does CSS stand for?",
+            a: "Central Style Sheets",
+            b: "Cascading Style Sheets",
+            c: "Cascading Simple Sheets",
+            d: "Cars SUVs Sailboats",
+            correct: "b",
+        },
+        {
+            question: "What does HTML stand for?",
+            a: "Hypertext Markup Language",
+            b: "Hypertext Markdown Language",
+            c: "Hyperloop Machine Language",
+            d: "Helicopters Terminals Motorboats Lamborginis",
+            correct: "a",
+        },
+        {
+            question: "What year was JavaScript launched?",
+            a: "1996",
+            b: "1995",
+            c: "1994",
+            d: "none of the above",
+            correct: "b",
+        },
+    ]
+}
+else if(document.getElementById('css'))
+{
+    quizData=  [
+        {
+            question: "Which language runs in a web browser abhinav 123?",
+            a: "Java",
+            b: "C",
+            c: "Python",
+            d: "JavaScript",
+            correct: "d",
+        },
+        {
+            question: "What does CSS stand for?",
+            a: "Central Style Sheets",
+            b: "Cascading Style Sheets",
+            c: "Cascading Simple Sheets",
+            d: "Cars SUVs Sailboats",
+            correct: "b",
+        },
+        {
+            question: "What does HTML stand for?",
+            a: "Hypertext Markup Language",
+            b: "Hypertext Markdown Language",
+            c: "Hyperloop Machine Language",
+            d: "Helicopters Terminals Motorboats Lamborginis",
+            correct: "a",
+        },
+        {
+            question: "What year was JavaScript launched?",
+            a: "1996",
+            b: "1995",
+            c: "1994",
+            d: "none of the above",
+            correct: "b",
+        },
+    ]
+}
+else if(document.getElementById('javascript'))
+{
+    quizData=  [
+        {
+            question: "Which language runs in a web browser abhinav 456?",
+            a: "Java",
+            b: "C",
+            c: "Python",
+            d: "JavaScript",
+            correct: "d",
+        },
+        {
+            question: "What does CSS stand for?",
+            a: "Central Style Sheets",
+            b: "Cascading Style Sheets",
+            c: "Cascading Simple Sheets",
+            d: "Cars SUVs Sailboats",
+            correct: "b",
+        },
+        {
+            question: "What does HTML stand for?",
+            a: "Hypertext Markup Language",
+            b: "Hypertext Markdown Language",
+            c: "Hyperloop Machine Language",
+            d: "Helicopters Terminals Motorboats Lamborginis",
+            correct: "a",
+        },
+        {
+            question: "What year was JavaScript launched?",
+            a: "1996",
+            b: "1995",
+            c: "1994",
+            d: "none of the above",
+            correct: "b",
+        },
+    ]
+}
+
+console.log(document.getElementById('html'));
+    const quiz = document.getElementById('quiz')
+    const answerEls = document.querySelectorAll('.answer')
+    const questionEl = document.getElementById('question')
+    const a_text = document.getElementById('a_text')
+    const b_text = document.getElementById('b_text')
+    const c_text = document.getElementById('c_text')
+    const d_text = document.getElementById('d_text')
+    const submitBtn = document.getElementById('submit')
+    
+    let currentQuiz = 0
+    let score = 0
+    
+    loadQuiz()
+    
+    function loadQuiz() {
+        deselectAnswers()
+    
+        const currentQuizData = quizData[currentQuiz]
+    
+        questionEl.innerText = currentQuizData.question
+        a_text.innerText = currentQuizData.a
+        b_text.innerText = currentQuizData.b
+        c_text.innerText = currentQuizData.c
+        d_text.innerText = currentQuizData.d
+    }
+    
+    function deselectAnswers() {
+        answerEls.forEach(answerEl => answerEl.checked = false)
+    }
+    
+    function getSelected() {
+        let answer
+    
+        answerEls.forEach(answerEl => {
+            if(answerEl.checked) {
+                answer = answerEl.id
+            }
+        })
+    
+        return answer
+    }
+    
+    submitBtn.addEventListener('click', () => {
+        const answer = getSelected()
+    
+        if(answer) {
+            if(answer === quizData[currentQuiz].correct) {
+                score++
+            }
+    
+            currentQuiz++
+    
+            if(currentQuiz < quizData.length) {
+                loadQuiz()
+            } else {
+
+                const name = localStorage.getItem('name');
+                localStorage.clear();
+                const percentage = (score/quizData.length)*100;
+
+                quiz.innerHTML = `
+                    <h1><center>🎉🎉Well Done ${name}🎉🎉<center></h1>
+                    <h2>You answered ${score} out Of ${quizData.length} questions correctly</h2>
+                    <h2>Your Efficiency is ${percentage}%.</h2>
+                    <a href="../templates/index.html"><button>Go To Home</button></a>
+                `
+            }
+        }
+    })
